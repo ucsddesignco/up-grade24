7d7defd8887e70bc1b36cda522cc244f9ac0e583
comnk <vhsiao36@gmail.com>
2024-05-19 23:43:37 -0700

    fixed styling, started from a mobile based approach and created a list of text objects to map onto page

--- src/app/pages/Apply/Apply.tsx
+++ src/app/pages/Apply/Apply.tsx
@@ -3,6 +3,7 @@
 import Footer from '@/components/Footer/Footer';
 import 'keen-slider/keen-slider.min.css';
 import './Apply.scss';
+import { applyInfo } from './apply-info';
 
 export default function Apply() {
   return (
@@ -10,49 +11,15 @@ export default function Apply() {
       <p className="breadcrumb">.05 / Apply</p>
       <h2>Why Apply?</h2>
       <hr className="line"></hr>
-      <div className="grid">
-        <div className="row">
-          <div className="grid-item">
-            <h3 className="question">
-              Create Meaningful Solutions for Social Impact Nonprofits
-            </h3>
-            <p>
-              Bring your visions to reality, design with intention,
-              problem-solve and overcome obstacles, and work to create lasting
-              change for a larger community!
-            </p>
-          </div>
-          <div className="grid-item">
-            <h3 className="question">UP-Grade your resume + portfolio</h3>
-            <p>
-              Gain real-world experience through a fully developed project and
-              stand out from other student designers with a personal story about
-              your involvement in the project process!
-            </p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="grid-item">
-            <h3 className="question">
-              Put classroom knowledge to real-world Use
-            </h3>
-            <p>
-              Bring your knowledge of technical and soft skills (and all things
-              design process related) to active and practical use, further
-              refining your skills along the way!
-            </p>
-          </div>
-          <div className="grid-item">
-            <h3 className="question">
-              Network with peers and industry professionals
-            </h3>
-            <p>
-              Collaborate with leaders in local nonprofit organizations, connect
-              with tech industry professionals, and get to know project mentors!
-            </p>
-          </div>
-        </div>
-      </div>
+      <ul className="grid">
+        {applyInfo.map(item => (
+          <li key={item.id}>
+            <h3>{item.header}</h3>
+            <p>{item.content}</p>
+          </li>
+        ))}
+        <li></li>
+      </ul>
       <hr className="line"></hr>
       <div className="testimonials">
         <h3>See some testimonials from previous participants:</h3>
