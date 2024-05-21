a39976e2ae022ad2e0d42eba6a247f6188897001
comnk <vhsiao36@gmail.com>
2024-05-21 12:57:54 -0700

    test

--- src/app/pages/Apply/Apply.tsx
+++ src/app/pages/Apply/Apply.tsx
@@ -2,10 +2,30 @@
 
 import Footer from '@/components/Footer/Footer';
 import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
+import { useState } from 'react';
 import './Apply.scss';
 import { applyInfo } from './apply-info';
+import { testimonialInfo } from './testimonial-info';
 
 export default function Apply() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+
+  const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    initial: currentSlide,
+    slides: {
+      perView: 1
+    },
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    created() {
+      setLoaded(true);
+    }
+  });
+
   return (
     <section id="apply">
       <p className="breadcrumb">.05 / Apply</p>
@@ -23,6 +43,37 @@ export default function Apply() {
       <hr className="line"></hr>
       <div className="testimonials">
         <h3>See some testimonials from previous participants:</h3>
+        <div className="navigation-wrapper">
+          <div ref={sliderRef} className="keen-slider">
+            {testimonialInfo.map(item => (
+              <div
+                className="keen-slider__slide testimonial-slide"
+                key={item.id}
+              >
+                <p className="testimonial-text">{item.testimonial}</p>
+                <p>- {item.reviewer}</p>
+              </div>
+            ))}
+          </div>
+          {loaded && instanceRef.current && (
+            <>
+              <button
+                onClick={(e: any) =>
+                  e.stopPropagation() || instanceRef.current?.prev()
+                }
+              >
+                &lt;
+              </button>
+              <button
+                onClick={(e: any) =>
+                  e.stopPropagation() || instanceRef.current?.next()
+                }
+              >
+                &gt;
+              </button>
+            </>
+          )}
+        </div>
       </div>
       <Footer />
     </section>
---
+++ src/app/pages/Apply/testimonial-info.ts
@@ -0,0 +1,20 @@
+export const testimonialInfo = [
+  {
+    id: 'apply-0',
+    testimonial:
+      '“Learning about marketing and social media branding was a plus for sure, I feel like I needed an intro to many of the topics covered and there was no better way to do so than taking a part of such a versatile and dedicated team that ranged in skills, talents, and industry knowledge.”',
+    reviewer: 'PAST UP-GRADER'
+  },
+  {
+    id: 'apply-1',
+    testimonial:
+      '“Learning about marketing and social media branding was a plus for sure, I feel like I needed an intro to many of the topics covered and there was no better way to do so than taking a part of such a versatile and dedicated team that ranged in skills, talents, and industry knowledge.”',
+    reviewer: 'PAST UP-GRADER'
+  },
+  {
+    id: 'apply-2',
+    testimonial:
+      '“Learning about marketing and social media branding was a plus for sure, I feel like I needed an intro to many of the topics covered and there was no better way to do so than taking a part of such a versatile and dedicated team that ranged in skills, talents, and industry knowledge.”',
+    reviewer: 'PAST UP-GRADER'
+  }
+];
