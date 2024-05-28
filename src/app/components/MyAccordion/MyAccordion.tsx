bc0dd24ed758025ad37f517293389206229a744b
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2024-05-28 15:43:39 -0700

    Squashed merge with page/EdwardNew/faq-page

---
+++ src/app/components/MyAccordion/MyAccordion.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import './MyAccordion.scss';
+import * as Accordion from '@radix-ui/react-accordion';
+import PlusIcon from '@/assets/icons/plus-icon.svg';
+import { useRef } from 'react';
+import { useSetAdjacentTriggerHeight } from './hooks/useSetAdjacentTriggerHeight';
+
+export type AccordionDataItem = {
+  id: string;
+  header: string;
+  content: string;
+};
+
+type AccordionTabProps = {
+  accordionData: Array<AccordionDataItem>;
+};
+
+const COLUMN_COUNT = 2;
+
+export default function MyAccordion({ accordionData }: AccordionTabProps) {
+  const triggerRefs = useRef<(HTMLButtonElement | null)[]>([]);
+
+  // Ensures that the height of adjacent accordion triggers are the same
+  useSetAdjacentTriggerHeight({ triggerRefs, accordionData, COLUMN_COUNT });
+
+  return (
+    <Accordion.Root className="AccordionRoot" type="single" collapsible={true}>
+      {[...Array(COLUMN_COUNT)].map((_column, columnIndex) => (
+        <div
+          className="AccordionColumn"
+          key={`accordion-col-${columnIndex}`}
+          tabIndex={-1}
+        >
+          {accordionData
+            .slice(
+              (columnIndex * accordionData.length) / COLUMN_COUNT,
+              (columnIndex * accordionData.length) / COLUMN_COUNT +
+                accordionData.length / COLUMN_COUNT
+            )
+            .map((item, rowIndex) => (
+              <Accordion.Item
+                className="AccordionItem"
+                value={`accordion-${item.id}`}
+                key={`accordion-${item.id}`}
+              >
+                <Accordion.Header>
+                  <Accordion.Trigger
+                    className="AccordionTrigger"
+                    ref={element => {
+                      triggerRefs.current[
+                        (columnIndex * accordionData.length) / 2 + rowIndex
+                      ] = element;
+                    }}
+                  >
+                    <span>{item.header}</span>
+                    <PlusIcon className="AccordionIcon" />
+                  </Accordion.Trigger>
+                </Accordion.Header>
+                <Accordion.Content className="AccordionContent">
+                  <p>{item.content}</p>
+                </Accordion.Content>
+              </Accordion.Item>
+            ))}
+        </div>
+      ))}
+    </Accordion.Root>
+  );
+}
--- src/app/pages/FAQ/FAQ.tsx
+++ src/app/pages/FAQ/FAQ.tsx
@@ -1,10 +1,13 @@
 import './FAQ.scss';
+import MyAccordion from '@/components/MyAccordion/MyAccordion';
+import { FAQContent } from './constants';
 
 export default function FAQ() {
   return (
     <section id="faq">
       <p className="breadcrumb">.04 / FAQ</p>
-      <h2>Commonly Asked Questions</h2>
+      <h2>Frequently Asked Questions</h2>
+      <MyAccordion accordionData={FAQContent} />
     </section>
   );
 }
