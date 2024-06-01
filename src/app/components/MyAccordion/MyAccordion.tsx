ec17394caf6b5c7ee2ac469c115a89c5079ff7f1
EdwardNew <pkarrows@gmail.com>
2024-06-01 11:49:45 -0700

    feat: add base functionality for expand all button

--- src/app/components/MyAccordion/MyAccordion.tsx
+++ src/app/components/MyAccordion/MyAccordion.tsx
@@ -3,7 +3,7 @@
 import './MyAccordion.scss';
 import * as Accordion from '@radix-ui/react-accordion';
 import PlusIcon from '@/assets/icons/plus-icon.svg';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useSetAdjacentTriggerHeight } from './hooks/useSetAdjacentTriggerHeight';
 
 export type AccordionDataItem = {
@@ -20,50 +20,73 @@ const COLUMN_COUNT = 2;
 
 export default function MyAccordion({ accordionData }: AccordionTabProps) {
   const triggerRefs = useRef<(HTMLButtonElement | null)[]>([]);
-
   // Ensures that the height of adjacent accordion triggers are the same
   useSetAdjacentTriggerHeight({ triggerRefs, accordionData, COLUMN_COUNT });
 
+  // const [isAllExpanded, setIsAllExpanded] = useState(false);
+  const [expandedItems, setExpandedItems] = useState<string[]>([]);
+
+  const handleExpandAll = () => {
+    if (expandedItems.length === accordionData.length) {
+      setExpandedItems([]); // Collapse all items
+    } else {
+      setExpandedItems(accordionData.map(item => `accordion-${item.id}`)); // Expand all items
+    }
+  };
+
   return (
-    <Accordion.Root className="AccordionRoot" type="single" collapsible={true}>
-      {[...Array(COLUMN_COUNT)].map((_column, columnIndex) => (
-        <div
-          className="AccordionColumn"
-          key={`accordion-col-${columnIndex}`}
-          tabIndex={-1}
-        >
-          {accordionData
-            .slice(
-              (columnIndex * accordionData.length) / COLUMN_COUNT,
-              (columnIndex * accordionData.length) / COLUMN_COUNT +
-                accordionData.length / COLUMN_COUNT
-            )
-            .map((item, rowIndex) => (
-              <Accordion.Item
-                className="AccordionItem"
-                value={`accordion-${item.id}`}
-                key={`accordion-${item.id}`}
-              >
-                <Accordion.Header>
-                  <Accordion.Trigger
-                    className="AccordionTrigger"
-                    ref={element => {
-                      triggerRefs.current[
-                        (columnIndex * accordionData.length) / 2 + rowIndex
-                      ] = element;
-                    }}
-                  >
-                    <span>{item.header}</span>
-                    <PlusIcon className="AccordionIcon" />
-                  </Accordion.Trigger>
-                </Accordion.Header>
-                <Accordion.Content className="AccordionContent">
-                  <p>{item.content}</p>
-                </Accordion.Content>
-              </Accordion.Item>
-            ))}
-        </div>
-      ))}
-    </Accordion.Root>
+    <>
+      <button onClick={handleExpandAll}>
+        {expandedItems.length === accordionData.length
+          ? 'Collapse All'
+          : 'Expand All'}
+      </button>
+
+      <Accordion.Root
+        className="AccordionRoot"
+        type={'multiple'}
+        value={expandedItems}
+        onValueChange={setExpandedItems}
+      >
+        {[...Array(COLUMN_COUNT)].map((_column, columnIndex) => (
+          <div
+            className="AccordionColumn"
+            key={`accordion-col-${columnIndex}`}
+            tabIndex={-1}
+          >
+            {accordionData
+              .slice(
+                (columnIndex * accordionData.length) / COLUMN_COUNT,
+                (columnIndex * accordionData.length) / COLUMN_COUNT +
+                  accordionData.length / COLUMN_COUNT
+              )
+              .map((item, rowIndex) => (
+                <Accordion.Item
+                  className="AccordionItem"
+                  value={`accordion-${item.id}`}
+                  key={`accordion-${item.id}`}
+                >
+                  <Accordion.Header>
+                    <Accordion.Trigger
+                      className="AccordionTrigger"
+                      ref={element => {
+                        triggerRefs.current[
+                          (columnIndex * accordionData.length) / 2 + rowIndex
+                        ] = element;
+                      }}
+                    >
+                      <span>{item.header}</span>
+                      <PlusIcon className="AccordionIcon" />
+                    </Accordion.Trigger>
+                  </Accordion.Header>
+                  <Accordion.Content className="AccordionContent">
+                    <p>{item.content}</p>
+                  </Accordion.Content>
+                </Accordion.Item>
+              ))}
+          </div>
+        ))}
+      </Accordion.Root>
+    </>
   );
 }
