1e5ada3f43c2f593e87e06b5c3282b49bd8e68e2
Aaron Chan <42254254+aaronchan32@users.noreply.github.com>
2024-06-01 17:44:40 -0700

    feat: add nonprofits and pomegranate

--- src/app/components/ThemesSlider/ThemeSlide/ThemeSlide.tsx
+++ src/app/components/ThemesSlider/ThemeSlide/ThemeSlide.tsx
@@ -1,24 +1,33 @@
 import './ThemeSlide.scss';
-import type { slideContent } from '../ThemesSlider';
+import type { SlideContent } from '../ThemesSlider';
 
 type content = {
-  content: slideContent;
+  content: SlideContent;
 };
 
 export default function ThemeSlide({ content }: content) {
   return (
     <div className="theme-slide">
-      <div>
+      <div className="top-section">
         <h3 style={{ backgroundColor: content.themeColor }}>{content.theme}</h3>
         <p>{content.description}</p>
       </div>
-      <div>
+      <div className="bottom-section">
         <h4>Non-profits</h4>
-        {content.nonProfits.map((nonProfit, idx) => (
-          <p key={idx} className="non-profit">
-            {nonProfit}
-          </p>
-        ))}
+        <hr />
+        <ul className="non-profit-container">
+          {content.nonProfits.map((nonProfit, idx) => (
+            <li key={idx} className="non-profit">
+              <a
+                href={nonProfit.link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {nonProfit.text}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
--- src/app/pages/Themes/Themes.tsx
+++ src/app/pages/Themes/Themes.tsx
@@ -1,14 +1,20 @@
 import './Themes.scss';
 import ThemesSlider from '@/components/ThemesSlider/ThemesSlider';
 import { ThemesContent } from './constants';
+import pomegranateImg from '@/assets/images/pomegranate.webp';
+import Image from 'next/image';
 
 export default function Themes() {
   return (
     <section id="themes">
       <p className="breadcrumb">.03 / Themes</p>
-      <h2>{`What are this year's UP-Grade themes?`}</h2>
+      <h2>{`Get a Taste Of Our Fresh Themes.`}</h2>
+      <hr className="title-line" />
       {/* <p>Swipe right to see more.</p> */}
       <ThemesSlider slidesContent={ThemesContent} />
+      <div className="pomegranate-container">
+        <Image src={pomegranateImg} alt="Pomegranate" className="pomegranate" />
+      </div>
     </section>
   );
 }
