eec6adfff230342e7657520ba54ba26eed90e146
EdwardNew <pkarrows@gmail.com>
2024-06-01 20:36:14 -0700

    feat: add styling for mobile layout

--- src/app/components/Fruits/Fruits.tsx
+++ src/app/components/Fruits/Fruits.tsx
@@ -1,15 +1,15 @@
 'use client';
 
 import './Fruits.scss';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import Matter, {
   Common,
   Mouse,
   MouseConstraint,
-  Vertices, 
+  Vertices,
   Events,
   Body,
-   Query, 
+  Query
 } from 'matter-js';
 
 import 'pathseg';
@@ -17,11 +17,8 @@ import createEllipseVertices from './createEllipseVertices';
 
 export default function Fruits() {
   const scene = useRef<HTMLDivElement>(null);
- 
+
   Common.setDecomp(require('poly-decomp'));
-
-
-
 
   useEffect(() => {
     if (!scene.current) return;
@@ -31,8 +28,7 @@ export default function Fruits() {
       Runner = Matter.Runner,
       Bodies = Matter.Bodies,
       Body = Matter.Body,
-      Composite = Matter.Composite
-
+      Composite = Matter.Composite;
 
     const engine = Engine.create();
     const render = Render.create({
@@ -47,11 +43,7 @@ export default function Fruits() {
       }
     });
 
-   // Import the necessary modules from Matter.js
-
-
-
-    
+    // Import the necessary modules from Matter.js
 
     engine.positionIterations = 10;
     engine.velocityIterations = 10;
@@ -60,7 +52,7 @@ export default function Fruits() {
 
       const width = scene.current?.clientWidth ?? 0; // Provide default value of 0 if undefined
       const height = scene.current?.clientHeight ?? 0; // Provide default value of 0 if undefined
-      
+
       render.bounds.max.x = width;
       render.bounds.max.y = height;
       render.options.width = width;
@@ -71,90 +63,133 @@ export default function Fruits() {
       render.canvas.style.height = height + 'px';
 
       console.log('Width:', width, 'Height:', height);
-      console.log( width / 2, height + barrierWidth / 2)
-      Body.setPosition(ground, { x: width / 2, y: (height + barrierWidth) / 2});
+      console.log(width / 2, height + barrierWidth / 2);
+      Body.setPosition(ground, {
+        x: width / 2,
+        y: (height + barrierWidth) / 2
+      });
 
-      Body.setPosition(rightWall, { x: (width + barrierWidth) / 2, y: height / 2 });
-      
+      Body.setPosition(rightWall, {
+        x: (width + barrierWidth) / 2,
+        y: height / 2
+      });
+
       Engine.update(engine, 0);
     };
 
-    
-
-
-    
     let barrierWidth = 500;
     const width = scene.current?.clientWidth ?? 0; // Provide default value of 0 if undefined
     const height = scene.current?.clientHeight ?? 0; // Provide default value of 0 if undefined
 
-    const ground = Bodies.rectangle(width/2, height + barrierWidth / 2 -10, 20000, barrierWidth, {
-      isStatic: true,
-      friction: 0.1, // Adjust this value, 0 means no friction
-      restitution: 0.1,
-      render: {
-        fillStyle: 'transparent',
+    const ground = Bodies.rectangle(
+      width / 2,
+      height + barrierWidth / 2 - 10,
+      20000,
+      barrierWidth,
+      {
+        isStatic: true,
+        friction: 0.1, // Adjust this value, 0 means no friction
+        restitution: 0.1,
+        render: {
+          fillStyle: 'transparent'
+        }
       }
-    });
-    const leftSlant = Bodies.rectangle(-5 - barrierWidth / 2, height/2, barrierWidth, height * 5, {
-      isStatic: true,
-      friction: 0.1, // Adjust this value, 0 means no friction
-      restitution: 0.1,
-      render: {
-        fillStyle: 'transparent',
+    );
+    const leftSlant = Bodies.rectangle(
+      -5 - barrierWidth / 2,
+      height / 2,
+      barrierWidth,
+      height * 5,
+      {
+        isStatic: true,
+        friction: 0.1, // Adjust this value, 0 means no friction
+        restitution: 0.1,
+        render: {
+          fillStyle: 'transparent'
+        }
       }
-    });
-    const rightSlant = Bodies.rectangle(width + barrierWidth / 2 + 5 , height / 2, barrierWidth, height * 5, {
-      isStatic: true,
-      
-      friction: 0.1, // Adjust this value, 0 means no friction
-      restitution: 0.1,
-      render: {
-        fillStyle: 'transparent',
+    );
+    const rightSlant = Bodies.rectangle(
+      width + barrierWidth / 2 + 5,
+      height / 2,
+      barrierWidth,
+      height * 5,
+      {
+        isStatic: true,
+
+        friction: 0.1, // Adjust this value, 0 means no friction
+        restitution: 0.1,
+        render: {
+          fillStyle: 'transparent'
+        }
       }
-    });
-    const leftWall = Bodies.rectangle(0 - barrierWidth / 2, height/2, barrierWidth, height * 5, {
-      isStatic: true,
-      friction: 0.1, // Adjust this value, 0 means no friction
-      restitution: 0.1,
-      render: {
-        fillStyle: 'transparent',
+    );
+    const leftWall = Bodies.rectangle(
+      0 - barrierWidth / 2,
+      height / 2,
+      barrierWidth,
+      height * 5,
+      {
+        isStatic: true,
+        friction: 0.1, // Adjust this value, 0 means no friction
+        restitution: 0.1,
+        render: {
+          fillStyle: 'transparent'
+        }
       }
-    });
-    const rightWall = Bodies.rectangle(width + barrierWidth / 2 , height / 2, barrierWidth, height * 5, {
-      isStatic: true,
-      
-      friction: 0.1, // Adjust this value, 0 means no friction
-      restitution: 0.1,
-      render: {
-        fillStyle: 'transparent',
+    );
+    const rightWall = Bodies.rectangle(
+      width + barrierWidth / 2,
+      height / 2,
+      barrierWidth,
+      height * 5,
+      {
+        isStatic: true,
+
+        friction: 0.1, // Adjust this value, 0 means no friction
+        restitution: 0.1,
+        render: {
+          fillStyle: 'transparent'
+        }
       }
-    });
+    );
     Body.rotate(leftSlant, -0.075 * Math.PI);
     Body.rotate(rightSlant, 0.075 * Math.PI);
     let spriteWidth = width / 815;
-    let spriteHeight = height * .5 / 385;
-    const basket = Bodies.rectangle(width / 2, height *.75, width, height * .5, {
-      isStatic: true,
-      friction: 0.1, // Adjust this value, 0 means no friction
-      restitution: 0.1,
-      render: {
-        sprite: {
-          texture: '/textures/basket.png',
-          xScale: spriteWidth,
-          yScale: spriteHeight,
-        },
-      },
-    });
+    let spriteHeight = (height * 0.5) / 385;
+    const basket = Bodies.rectangle(
+      width / 2,
+      height * 0.75,
+      width,
+      height * 0.5,
+      {
+        isStatic: true,
+        friction: 0.1, // Adjust this value, 0 means no friction
+        restitution: 0.1,
+        render: {
+          sprite: {
+            texture: '/textures/basket.png',
+            xScale: spriteWidth,
+            yScale: spriteHeight
+          }
+        }
+      }
+    );
     basket.collisionFilter = {
-      'group': -1,
-      'category': 2,
-      'mask': 0,
+      group: -1,
+      category: 2,
+      mask: 0
     };
 
     // Matter.Body.rotate(leftWall, -0.08 * Math.PI);
     // Matter.Body.rotate(rightWall, 0.08 * Math.PI);
-    Composite.add(engine.world, [ground, leftWall, rightWall, leftSlant, rightSlant]);
-    
+    Composite.add(engine.world, [
+      ground,
+      leftWall,
+      rightWall,
+      leftSlant,
+      rightSlant
+    ]);
 
     const words = [
       {
@@ -187,16 +222,15 @@ export default function Fruits() {
         textHeight: 60,
         boxScale: 1.5,
         spriteScale: 0.8,
-        svgPath: '/textures/wordFour.svg',
+        svgPath: '/textures/wordFour.svg'
       }
     ];
 
-   
     // Create Words
- 
+
     words.forEach((word, index) => {
       const { textWidth, textHeight, boxScale, spriteScale, svgPath } = word;
-      const posX = (index) * 200 + 200;
+      const posX = index * 200 + 200;
       const posY = -200;
       let width = textWidth * boxScale;
       let height = textHeight * boxScale;
@@ -211,11 +245,11 @@ export default function Fruits() {
           strokeStyle: 'black',
           fillStyle: 'black',
           lineWidth: 1,
-        
+
           sprite: {
             texture: svgPath,
             xScale: spriteScale,
-            yScale: spriteScale ,
+            yScale: spriteScale
           }
         }
       });
@@ -223,92 +257,104 @@ export default function Fruits() {
       Composite.add(engine.world, rectangle);
     });
 
-  
-
     //Create Cherry
     let cherryScale = 0.15;
 
     const cherryShape = createEllipseVertices({
       cx: 0,
       cy: 0,
-      ry: scene.current?.clientWidth * cherryScale* 1,
-      rx: scene.current?.clientWidth * cherryScale*.6,
+      ry: scene.current?.clientWidth * cherryScale * 1,
+      rx: scene.current?.clientWidth * cherryScale * 0.6,
       steps: 20
     });
     const textureWidth = 150;
-    const textureHeight = 100;
+    // const textureHeight = 100;
     const cherry = Bodies.fromVertices(
-      scene.current?.clientWidth*.3, scene.current?.clientHeight * -0.2, 
+      scene.current?.clientWidth * 0.3,
+      scene.current?.clientHeight * -0.2,
       [Vertices.hull(cherryShape)],
       {
-      restitution: 0.6, //Bounciness
-      render: {
-        fillStyle: 'black',
-        sprite: {
-          texture: '/textures/cherry.png',
-          xScale: (cherryScale * scene.current?.clientWidth * 1.5) / textureWidth,
-          yScale: (cherryScale * scene.current?.clientWidth * 1.5 ) / textureWidth,
-        },
-      },
-        });
-        Composite.add(engine.world, cherry);
+        restitution: 0.6, //Bounciness
+        render: {
+          fillStyle: 'black',
+          sprite: {
+            texture: '/textures/cherry.png',
+            xScale:
+              (cherryScale * scene.current?.clientWidth * 1.5) / textureWidth,
+            yScale:
+              (cherryScale * scene.current?.clientWidth * 1.5) / textureWidth
+          }
+        }
+      }
+    );
+    Composite.add(engine.world, cherry);
 
-      const watermelonScale = 0.25;
-      const watermelonShape = createEllipseVertices({
-        cx: 0,
-        cy: 0,
-        ry: scene.current?.clientWidth * watermelonScale* 1.2,
-        rx: scene.current?.clientWidth * watermelonScale*1,
-        steps: 16
-      });
-      const watermelon = Bodies.fromVertices(
-        scene.current?.clientWidth *.6, scene.current?.clientHeight * -0.2, 
-        [Vertices.hull(watermelonShape)],
-        {
+    const watermelonScale = 0.25;
+    const watermelonShape = createEllipseVertices({
+      cx: 0,
+      cy: 0,
+      ry: scene.current?.clientWidth * watermelonScale * 1.2,
+      rx: scene.current?.clientWidth * watermelonScale * 1,
+      steps: 16
+    });
+    const watermelon = Bodies.fromVertices(
+      scene.current?.clientWidth * 0.6,
+      scene.current?.clientHeight * -0.2,
+      [Vertices.hull(watermelonShape)],
+      {
         restitution: 0.6, //Bounciness
         render: {
           fillStyle: 'black',
           sprite: {
             texture: '/textures/watermelon.webp',
-            xScale: (watermelonScale * scene.current?.clientWidth * 0.4) / textureWidth,
-            yScale: (watermelonScale * scene.current?.clientWidth * 0.4 ) / textureWidth,
-          },
-        },
-          });
-          Composite.add(engine.world, watermelon);
+            xScale:
+              (watermelonScale * scene.current?.clientWidth * 0.4) /
+              textureWidth,
+            yScale:
+              (watermelonScale * scene.current?.clientWidth * 0.4) /
+              textureWidth
+          }
+        }
+      }
+    );
+    Composite.add(engine.world, watermelon);
 
-          const apricotScale = 0.25;
-          const apricotShape = createEllipseVertices({
-            cx: 0,
-            cy: 0,
-            ry: scene.current?.clientHeight * apricotScale*.9,
-            rx: scene.current?.clientWidth * apricotScale*.8,
-            steps: 20
-          });
-          const apricot = Bodies.fromVertices(
-            scene.current?.clientWidth*.2, scene.current?.clientHeight * -.8, 
-            [Vertices.hull(apricotShape)],
-            {
-            restitution: 0.6, //Bounciness
-            render: {
-              fillStyle: 'black',
-              sprite: {
-                texture: '/textures/apricot.webp',
-                xScale: (apricotScale * scene.current?.clientWidth * .45) / textureWidth,
-                yScale: (apricotScale * scene.current?.clientWidth * .45 ) / textureWidth,
-              },
-            },
-              });
-              Composite.add(engine.world, apricot);
+    const apricotScale = 0.25;
+    const apricotShape = createEllipseVertices({
+      cx: 0,
+      cy: 0,
+      ry: scene.current?.clientHeight * apricotScale * 0.9,
+      rx: scene.current?.clientWidth * apricotScale * 0.8,
+      steps: 20
+    });
+    const apricot = Bodies.fromVertices(
+      scene.current?.clientWidth * 0.2,
+      scene.current?.clientHeight * -0.8,
+      [Vertices.hull(apricotShape)],
+      {
+        restitution: 0.6, //Bounciness
+        render: {
+          fillStyle: 'black',
+          sprite: {
+            texture: '/textures/apricot.webp',
+            xScale:
+              (apricotScale * scene.current?.clientWidth * 0.45) / textureWidth,
+            yScale:
+              (apricotScale * scene.current?.clientWidth * 0.45) / textureWidth
+          }
+        }
+      }
+    );
+    Composite.add(engine.world, apricot);
 
-          // Get the canvas element
+    // Get the canvas element
     const canvas = scene.current;
 
     if (canvas) {
       // Add a mouse click event listener to the canvas
       canvas.addEventListener('click', () => {
         console.log('Clicked');
-        Composite.allBodies(engine.world).forEach((body) => {
+        Composite.allBodies(engine.world).forEach(body => {
           // For each fruit
           if (body.isStatic) return; // Skip static bodies
 
@@ -322,7 +368,7 @@ export default function Fruits() {
           Body.applyForce(body, body.position, force);
         });
       });
-}
+    }
 
     const mouse = Mouse.create(render.canvas);
     const mouseConstraint = MouseConstraint.create(engine, {
@@ -339,9 +385,7 @@ export default function Fruits() {
 
     Composite.add(engine.world, mouseConstraint);
 
-
     // ...
-
 
     interface Event {
       source: {
@@ -356,14 +400,14 @@ export default function Fruits() {
         let maxSpeed: number = 10;
         Matter.Body.setVelocity(body, {
           x: Math.min(maxSpeed, Math.max(-maxSpeed, body.velocity.x)),
-          y: Math.min(maxSpeed, Math.max(-maxSpeed, body.velocity.y)),
+          y: Math.min(maxSpeed, Math.max(-maxSpeed, body.velocity.y))
         });
       });
     };
-    Matter.Events.on(engine, 'beforeUpdate', limitMaxSpeed)
+    Matter.Events.on(engine, 'beforeUpdate', limitMaxSpeed);
 
     let barriers = [ground, leftWall, rightWall];
-    Events.on(mouseConstraint, 'startdrag', (event) => {
+    Events.on(mouseConstraint, 'startdrag', event => {
       const body = event.body;
 
       // Store the original inertia of the body
@@ -379,7 +423,7 @@ export default function Fruits() {
       }
     });
 
-    Events.on(mouseConstraint, 'enddrag', (event) => {
+    Events.on(mouseConstraint, 'enddrag', event => {
       const body = event.body;
 
       // Reset the inertia back to its original value
@@ -390,9 +434,6 @@ export default function Fruits() {
       // Remove the original inertia from the body
       delete body.originalInertia;
     });
-
-
-
 
     Render.run(render);
     const runner = Runner.create();
@@ -410,7 +451,7 @@ export default function Fruits() {
 
   return (
     <>
-      <div ref={scene} className="fruit-container"></div>;
+      <div ref={scene} className="fruit-container"></div>
     </>
   );
 }
--- src/app/pages/Landing/Landing.tsx
+++ src/app/pages/Landing/Landing.tsx
@@ -4,8 +4,21 @@ import Fruits from '@/components/Fruits/Fruits';
 export default function Landing() {
   return (
     <section id="landing">
+      <div className="mobile-header">
+        <h1>up-grade 2024</h1>
+        <p>UCSD Design Co</p>
+        <p>San Diego, CA</p>
+        <h3>june 1st to august 13th</h3>
+      </div>
       <p className="breadcrumb">.01 / Home</p>
       <Fruits />
+      {/* <Signature
+        navContainerRef={navContainerRef}
+        hoveringCart={hoveringCart}
+      /> */}
+      <a className="add-me-to-cart">
+        <p>ADD ME TO CART</p>
+      </a>
     </section>
   );
 }
